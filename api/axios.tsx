7bebea4d24d94899a1d4c857ec2e445d41d6fde3
Vanilin123 <149298214+Vanilin123@users.noreply.github.com>
2024-09-09 19:14:35 +0300

    Update axios.tsx

--- api/axios.tsx
+++ api/axios.tsx
@@ -8,7 +8,7 @@ import {
 import { authService } from '../services/auth.service'
 
 const options: CreateAxiosDefaults = {
-	baseURL: 'http://localhost:4200/api',
+	baseURL: 'http://vanilin123-mchl-back-rep-ce3d.twc1.net/api',
 	headers: {
 		'Content-Type': 'application/json'
 	},
